f883b90e7b0f3b6cb2a1bf9eff433fa328c9706a
Oliver <ocaine@technophobia.com>
2012-06-20 12:09:42 +0100

    offline use

---
+++ mvvm.js
@@ -0,0 +1,26 @@
+﻿var SimpleListModel = function () {
+    this.items = ko.observableArray([]);
+    this.itemToAdd = ko.observable("");
+    this.addItem = function () {
+        this.items.push(this.itemToAdd());
+        this.itemToAdd("");
+    } .bind(this);
+};
+
+$(function () {
+    var storageKey = "model";
+    var viewModel = new SimpleListModel();
+
+    if (localStorage.getItem(storageKey)) {
+        var localViewModel = ko.mapping.fromJSON(localStorage.getItem(storageKey));
+        viewModel.items(localViewModel.items());
+    }
+
+    ko.applyBindings(viewModel);
+
+    var storeViewModel = function () {
+        localStorage.setItem(storageKey, ko.toJSON(viewModel));
+    };
+
+    setInterval(storeViewModel, 2000);
+});
